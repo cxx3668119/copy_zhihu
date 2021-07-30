407905a69f449ebabe69cb1005c7375400928125
cxx3668119 <862704268@qq.com>
2021-07-30 13:32:20 +0800

    GlobalHeader组件完成

--- zheye/src/App.vue
+++ zheye/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list"> </column-list>
   </div>
 </template>
@@ -8,6 +9,14 @@
 import { defineComponent } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
+
+const currentUser: UserProps = {
+  // isLogin: true,
+  // name: "cxx",
+  // id: 1,
+  isLogin: false,
+};
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -40,10 +49,12 @@ export default defineComponent({
   name: "App",
   components: {
     ColumnList,
+    GlobalHeader,
   },
   setup() {
     return {
       list: testData,
+      currentUser: currentUser,
     };
   },
 });
---
+++ zheye/src/components/GlobalHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <a href="#" class="navbar-brand">者也专栏</a>
+    <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2 mr-2">登录</a>
+        <a href="#" class="btn btn-outline-light my-2">注册</a>
+      </li>
+    </ul>
+    <ul v-else class="list-inline mb-0">
+      <a href="#" class="btn btn-outline-light my-2">你好 {{ user.name }}</a>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+// 接口规范
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+}
+
+export default defineComponent({
+  name: "GlobalHeader",
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang='less' scoped>
+</style>
